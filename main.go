b1b45ed50786715f6fcb5c408e50007386f85961
Gonzalo Escobar <gescobar@technisys.com>
2020-08-08 19:57:10 -0300

    Agrego conexion a mongodb

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+	"github.com/gonzaloescobar/twitter-of-the-salty/handlers"
+	"github.com/gonzaloescobar/twitter-of-the-salty/db"
+)
+
+
 func main()  {
-	
+	if db.CheckConnection() == 0{
+		log.Fatal("Connection refused")
+		return
+	}
+	handlers.Handlers()
 }
